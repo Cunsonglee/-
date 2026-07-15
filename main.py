1164f4dca74107199319361f95198117ecebadd6
Cunsonglee <leecunsong@gmail.com>
2026-07-15 12:34:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -633,7 +633,7 @@ def scrape_website(url):
 def main(days=3):
     print(f"Iniciando extracción de noticias de los últimos {days} días...")
     all_posts = []
-    cutoff = datetime.now() - timedelta(days=days-1)
+    cutoff = datetime.now() - timedelta(days=days-59)
 
     for url in websites:
         posts = scrape_website(url)
